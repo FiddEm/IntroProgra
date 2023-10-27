a38ea39108e18cc71c77ef5da128b3004894abf8
BeansReal1 <ignavent@gmail.com>
2023-10-27 13:20:21 -0300

    Integracion de la pagina ayuda (no se si funciona)

--- paginaweb/paginaweb/views.py
+++ paginaweb/paginaweb/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render, redirect
 def home(req):
     ctx = {'nombre':'alo'}
     return render(req, 'test.html', ctx) 
+
+def ayuda(req):
+    ctx = {'nose':'que'}
+    return render(req, 'ayuda.html', ctx)
